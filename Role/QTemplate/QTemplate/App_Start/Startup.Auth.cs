fe06e8a6cd8a398321a3643ae0bda7ac1fd58553
Sefa Dudu <77778888+SefaDudu@users.noreply.github.com>
2021-07-21 09:56:41 +0300

    Login
    User And Admin Login

---
+++ Role/QTemplate/QTemplate/App_Start/Startup.Auth.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(QTemplate.App_Start.Startup))]
+
+namespace QTemplate.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });  
+        }
+    }
+}
---
+++ Role/QTemplate/QTemplate/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QTemplate.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username == "sefa" && password == "123123" ||
+                username == "admin" && password == "123123")
+            {
+
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Name, username)
+                };
+
+                if (username == "sefa")
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, "User"));
+                }
+                else if (username == "admin")
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+                }
+
+                var identiy = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+
+                AuthenticationManager.SignIn(identiy);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
